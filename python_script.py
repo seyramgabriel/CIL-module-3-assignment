3ed4300fd722a1e1d91094d6bdc50635615204fa
Seyram Gabriel <seyramgabriel@gmail.com>
2023-09-24 03:57:12 -0700

    python script to download s3 bucket content

--- python_script.py
+++ python_script.py
@@ -2,25 +2,22 @@
 import boto3
 import os
 
-#Set your AWS credentials and region
-aws_access_key_id = ''
-aws_secret_access_key = ''
-aws_region = ''
-
 # Set the S3 bucket name and local directory path
 s3_bucket_name = 'kofiseysey'  # Replace with your S3 bucket name
 local_directory = '/home/ssm-user/mys3backup'
 
 def copy_s3_bucket_contents():
-    # Initialize the S3 client
-    s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=aws_region)
-
     try:
+        # Initialize the S3 client with instance profile
+        session = boto3.Session()
+        s3 = session.client('s3', region_name='us-east-2')
+
+        # Check if the local directory exists, create it if not
+        if not os.path.exists(local_directory):
+            os.makedirs(local_directory)
+
         # List objects in the S3 bucket
         objects = s3.list_objects_v2(Bucket=s3_bucket_name)
-
-        # Create the local directory if it doesn't exist
-        os.makedirs(local_directory, exist_ok=True)
 
         # Iterate through S3 objects and copy them to the local directory
         for obj in objects.get('Contents', []):
@@ -29,14 +26,12 @@ def copy_s3_bucket_contents():
 
             # Download the S3 object
             s3.download_file(s3_bucket_name, s3_object_key, local_file_path)
-            print(f'Copied: {s3_object_key} to {local_file_path}')
+            print('Copied: {} to {}'.format(s3_object_key, local_file_path))
 
         print('S3 bucket contents copied to the local directory successfully.')
 
     except Exception as e:
-        print(f'Error: {e}')
+        print('Error: {}'.format(e))
 
 if __name__ == '__main__':
     copy_s3_bucket_contents()
-
-
